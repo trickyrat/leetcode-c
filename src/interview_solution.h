63fe35d65c6b8c4a49cf79f6d6d82ffce52dbfa3
trickyrat <wangjian0513@outlook.com>
2024-05-15 21:18:18 +0800

    chore: reformat function comments

--- src/interview_solution.h
+++ src/interview_solution.h
@@ -2,17 +2,17 @@
 
 #include <stdbool.h>
 
-/**
- * 01.09. String Rotation LCCI
- * @param s1
- * @param s2
- * @return
- */
+/// <summary>
+/// 01.09. String Rotation LCCI
+/// </summary>
+/// <param name="s1"></param>
+/// <param name="s2"></param>
+/// <returns></returns>
 bool is_flipped_string(char *s1, char *s2);
 
-/**
- * 17.09. Get Kth Magic Number LCCI
- * @param k
- * @return
- */
+/// <summary>
+/// 17.09. Get Kth Magic Number LCCI
+/// </summary>
+/// <param name="k"></param>
+/// <returns></returns>
 int get_kth_magic_number(int k);
